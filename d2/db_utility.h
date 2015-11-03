732d7318532d594ee5690489db339c4b1f49af59
yubin <someday_yubin@hotmail.com>
2015-11-03 01:17:16 -0500

    work

--- d2/db_utility.h
+++ d2/db_utility.h
@@ -28,7 +28,7 @@ public:
     void DBRemove_Project(string,project_obj*);
     void DBModify_Student(string,student_obj*);
     void DBModify_Project(string,project_obj*);
-    void DBSearch_Admin(string, string, admin);
+    //void DBSearch_Admin(string, string, admin);
 };
 
 #endif // DB_UTILITY_H
