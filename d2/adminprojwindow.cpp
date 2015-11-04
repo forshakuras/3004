2cfeb4ad5b71d75967e93b90e7d6050d4657d6ae
allen <forshakuras@gmail.com>
2015-11-04 02:30:56 -0500

    adminwindow functions and readme improvement

--- d2/adminprojwindow.cpp
+++ d2/adminprojwindow.cpp
@@ -1,6 +1,6 @@
 #include "adminprojwindow.h"
 #include "ui_adminprojwindow.h"
-
+#include "db_utility.h"
 AdminProjWindow::AdminProjWindow(QWidget *parent,project_obj *project) :
     QDialog(parent),
     ui(new Ui::AdminProjWindow)
@@ -26,7 +26,35 @@ AdminProjWindow::AdminProjWindow(QWidget *parent,project_obj *project) :
     ui->projDesc->setText(QString::fromStdString(projob->getDescription()));
     ui->projName->setText(QString::fromStdString(projob->getTitle()));
 }
+void AdminProjWindow::on_saveButton_clicked()
+{
+    projob->setAlgor(ui->algSpin->value());
+    projob->setCCpp(ui->cSpin->value());
+    projob->setGpa(ui->gpaSpin->value());
+    projob->setDatabase(ui->dbSpin->value());
+    projob->setPmSkill(ui->pmSpin->value());
+    projob->setJsHtml(ui->jsSpin->value());
+    projob->setPython(ui->pythonSpin->value());
+    projob->setDebug(ui->debugSpin->value());
+    projob->setDocument(ui->docSpin->value());
+    projob->setMobile(ui->mobileSpin->value());
+    projob->setUi(ui->uiSpin->value());
+    projob->setId(ui->id->text().toInt());
+    projob->setIdealNumberStudents(ui->idealSpin->value());
+    projob->setMaxStudents(ui->maxSpin->value());
+    projob->setTitle(ui->projName->text().toStdString());
+    projob->setDescription(ui->projDesc->text().toStdString());
+    DB_Utility *db;
+    db = new DB_Utility();
+    //db->DBModify_Project("projects",projob);
+}
 
+void AdminProjWindow::on_deleteButton_clicked()
+{
+    DB_Utility *db;
+    db = new DB_Utility();
+    db->DBRemove_Project("projects",projob);
+}
 AdminProjWindow::~AdminProjWindow()
 {
     delete ui;
